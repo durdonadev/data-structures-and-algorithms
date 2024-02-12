c84553924732edcf737d7051289d384d5da12610
Durdona Ibragimova <durdona.dev@gmail.com>
2024-02-12 10:30:56 -0500

    Funchtion that checks if there dublicate values in array

---
+++ chapter-4/02_hasDublicateValue.js
@@ -0,0 +1,14 @@
+// The function hasDuplicateValue that takes an array of integers as input and returns true if the array contains any duplicate values; otherwise, it returns false. (page 56)
+
+function hasDublicateValue(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (i !== j && array[i] === array[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(hasDublicateValue([1, 5, 3, 9, 1, 4]));
