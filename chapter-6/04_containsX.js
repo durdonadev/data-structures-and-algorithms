c21784d4e5d648b49a92840929be6891884931ad
Durdona Ibragimova <durdona.dev@gmail.com>
2024-02-27 14:10:15 -0500

    Implement contains X function that check wether string contains X

---
+++ chapter-6/04_containsX.js
@@ -0,0 +1,13 @@
+// The following function returns whether or not a capital “X” is present within a string.
+
+function containsX(string) {
+    let foundX = false;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "X") {
+            foundX = true;
+        }
+    }
+    return foundX;
+}
+
+console.log(containsX("eXam"));
